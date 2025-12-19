2402472c50122dee595f4417f6bac0ba05ea6eea
ireneiuorio <ireneiuorio@gmail.com>
2025-12-19 12:38:52 +0100

    Struttura Maven definitiva e gitignore configurato

---
+++ src/main/java/TestTomcat.java
@@ -0,0 +1,19 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class TestTomcat extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Tomcat 11 funziona correttamente!</h1>");
+        out.println("<p>Versione Java: " + System.getProperty("java.version") + "</p>");
+        out.println("<p>Se vedi questo, il tuo progetto Maven è configurato bene.</p>");
+        out.println("</body></html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Tickema - Home</title>
+</head>
+<body>
+<h1>Benvenuti in Tickema!</h1>
+<p>Se vedi questa pagina, Tomcat 11 e Maven sono configurati correttamente.</p>
+<p>Sistema Operativo attuale: <b><%= System.getProperty("os.name") %></b></p>
+</body>
+</html>
