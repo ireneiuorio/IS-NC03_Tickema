9bc1555e7cd473450b12573ce9af0037907b8d77
lpascarella5-rgb <l.pascarella5@studenti.unisa.it>
2025-12-30 20:04:24 +0100

    ciao

--- src/main/java/exception/sgu/PasswordErrataException.java
+++
@@ -1,7 +0,0 @@
-package exception.sgu;
-
-public class PasswordErrataException extends Exception {
-    public PasswordErrataException() {
-        super("La password inserita è errata.");
-    }
-}
